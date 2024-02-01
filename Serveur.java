7074e5d98230d5ae7941bbc55e046c3ecbb7b0da
nadiaames <116350633+nadiaames@users.noreply.github.com>
2024-02-01 11:17:57 +0100

    cd..

--- Serveur.java
+++ Serveur.java
@@ -8,7 +8,8 @@ public class Serveur {
 
     private static ServerSocket serverSocket, dataServer;
     private static Socket clientSocket, dataSocket;
-    private static String currentDirectory = System.getProperty("user.home");
+    private static boolean auth = true;
+    private static String currentDirectory = System.getProperty("user.dir");
 
     public static void main(String[] args) {
         try {
@@ -49,17 +50,34 @@ public class Serveur {
                 System.out.println("Recieved : " + command);
 
                 String[] commandParts = command.split(" ");
+                String msgLog = "421 Service not available, login first\r\n";
 
                 if (command.startsWith("USER")) {
                     handleUserAuthentication(commandParts, out, scanner);
                 } else if (command.equals("EPSV")) {
-                    handleEpsvCommand(out);
+                    if (auth == true) {
+                        handleEpsvCommand(out);
+                    } else {
+                        out.write(msgLog.getBytes());
+                    }
                 } else if (command.startsWith("RETR")) {
-                    handleRetrCommand(commandParts, out);
+                    if (auth == true) {
+                        handleRetrCommand(commandParts, out);
+                    } else {
+                        out.write(msgLog.getBytes());
+                    }
                 } else if (command.equals("LIST")) {
-                    handleListCommand(out);
+                    if (auth == true) {
+                        handleListCommand(out);
+                    } else {
+                        out.write(msgLog.getBytes());
+                    }
                 } else if (command.startsWith("CWD")) {
-                    handleCwdCommand(commandParts, out);
+                    if (auth == true) {
+                        handleCwdCommand(commandParts, out);
+                    } else {
+                        out.write(msgLog.getBytes());
+                    }
                 } else if (command.toUpperCase().equals("QUIT")) {
                     out.write("221 User logged out\r\n".getBytes());
                     if (clientSocket != null) clientSocket.close();
@@ -74,20 +92,25 @@ public class Serveur {
     }
 
     private static void handleUserAuthentication(String[] commandParts, OutputStream out, Scanner scanner) throws IOException {
-            String username = commandParts[1];
-            if ("nadia".equalsIgnoreCase(username)) {
-                out.write("331 User name okay, need password\r\n".getBytes());
-                String passCommand = scanner.nextLine();
-                String[] passParts = passCommand.split(" ");
-                if (passParts[0].equalsIgnoreCase("PASS") && "nad".equals(passParts[1])) {
-                    out.write("230 User logged in\r\n".getBytes());
-                } else {
-                    out.write("530 Not logged in, incorrect password\r\n".getBytes());
-                }
+        String username = commandParts[1];
+
+        if ("nadia".equalsIgnoreCase(username)) {
+            out.write("331 User name okay, need password\r\n".getBytes());
+            String passCommand = scanner.nextLine();
+            String[] passParts = passCommand.split(" ");
+            if (passParts[0].equalsIgnoreCase("PASS") && "nad".equals(passParts[1])) {
+                out.write("230 User logged in\r\n".getBytes());
             } else {
-                out.write("530 Not logged in, user not found\r\n".getBytes());
+                out.write("530 Not logged in, incorrect password\r\n".getBytes());
+                auth = false;
             }
+        } else {
+            out.write("530 Not logged in, user not found\r\n".getBytes());
+            auth = false;
+        }
+
     }
+    
 
     private static void handleEpsvCommand(OutputStream out) throws IOException {
         dataServer = new ServerSocket(0); 
@@ -116,46 +139,49 @@ public class Serveur {
                 dataOut.write(buffer, 0, count);
             }
             out.write("226 Transfered with success\r\n".getBytes());
-        } finally {
-            if (dataSocket != null && !dataSocket.isClosed()) {
-                dataSocket.close();
-            }
-            if (dataServer != null && !dataServer.isClosed()) {
-                dataServer.close();
-            }
-        }
+        } 
     }
-
+    
     private static void handleListCommand(OutputStream out) throws IOException {
         try (Socket dataSocket = dataServer.accept();
         OutputStream dataOut = dataSocket.getOutputStream()) {
 
-        File directoryCurrent = new File(currentDirectory);
-        File[] files = directoryCurrent.listFiles();
-        
-        if (files != null) {
-            out.write("150 Displaying the directory contents\r\n".getBytes());
+            File directoryCurrent = new File(currentDirectory);
+            File[] files = directoryCurrent.listFiles();
+            
+            if (files != null) {
+                out.write("150 Displaying the directory contents\r\n".getBytes());
 
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    dataOut.write(("d " + file.getName() + "\r\n").getBytes());
-                } else {
-                    dataOut.write(("- " + file.getName() + "\r\n").getBytes());
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        dataOut.write(("d " + file.getName() + "\r\n").getBytes());
+                    } else {
+                        dataOut.write(("- " + file.getName() + "\r\n").getBytes());
+                    }
                 }
+                out.write("226 Directory listing sent successfully.\r\n".getBytes());
+            } else {
+                out.write("550 Directory not found\r\n".getBytes());
+                return;
             }
-            out.write("226 Directory listing sent successfully.\r\n".getBytes());
-        } else {
-            out.write("550 Directory not found\r\n".getBytes());
-            return;
-        }
-    
-        }
+        } 
     }
     
     private static void handleCwdCommand(String[] commandParts, OutputStream out) throws IOException {
         String newDirectory = commandParts[1];
-        File newDir = new File(currentDirectory, newDirectory);
-    
+        File newDir = new File(currentDirectory, newDirectory);        
+        if ("..".equals(newDirectory)) {
+            if (currentDirectory.equals(System.getProperty("user.dir"))) {
+                out.write(("550 Requested action not taken. Cannot move beyond root directory.\r\n").getBytes());
+                return;
+            } else {
+                File parentDir = new File(currentDirectory).getParentFile();
+                currentDirectory = parentDir.getAbsolutePath();
+                out.write(("250 CWD successful. Current directory is " + currentDirectory + "\r\n").getBytes());
+                return;
+            }
+        }
+
         if (newDir.exists() && newDir.isDirectory()) {
             currentDirectory = newDir.getAbsolutePath();
             out.write(("250 CWD successful. Current directory is " + currentDirectory + "\r\n").getBytes());
@@ -164,5 +190,5 @@ public class Serveur {
         }
     }
     
-    
+
 }
